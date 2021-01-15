8f708c4bb04d425fec2e2187bab5e6e68454550b
Sir_Altruist <eshooluwasegun98@gmail.com>
2021-01-15 12:12:03 +0100

    unable to retain current state on page refresh

--- src/store.js
+++ src/store.js
@@ -1,7 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducers/index";
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("state", serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem("state");
+    if (!serializedState) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
+
+const persistedState = loadFromLocalStorage();
+
 const store = configureStore({
   reducer: rootReducer,
+  persistedState,
 });
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 export default store;
