d209c20c6b8d18509446ead40fcafff67ea1f27f
Sir-Altruist <eshooluwasegun98@gmail.com>
2022-06-21 06:00:36 +0100

    Fix-Build: [CS-75] Update Links to Privacy Policy and Terms

--- src/components/Basic/Footer/Footer4.js
+++ src/components/Basic/Footer/Footer4.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { List } from "semantic-ui-react";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Footer4({ darkMode }) {
   const handleTerms = () => {
--- src/components/Modals/index.js
+++
@@ -1,6 +0,0 @@
-import Terms from "./Terms";
-import TermsContent from "./Terms";
-import Privacy from "./Privacy";
-import PrivacyContent from "./PrivacyContent";
-
-export { Terms, TermsContent, Privacy, PrivacyContent };
