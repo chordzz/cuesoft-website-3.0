53396f706b6b03a54b6bd8b01acb4f2f9d9aa093
chordzz <52765542+chordzz@users.noreply.github.com>
2022-08-13 18:41:15 +0100

    fixed cookie alert responsiveness (#76)
    Signed-off-by: Ibukun Dairo <ibukun.dairo@cuesoft.io>
    Co-authored-by: Ibukun Dairo <ibukun.dairo@cuesoft.io>

--- src/components/cookie-alert/CookieAlert.tsx
+++ src/components/cookie-alert/CookieAlert.tsx
@@ -3,18 +3,18 @@ import Cookie from "../../assets/imgs/cookie.svg";
 
 export const CookieAlert = () => {
   return (
-    <div className="my-container flex flex-col md:flex-row items-center justify-between">
+    <div className="my-container flex flex-col md:flex-row items-center justify-between gap-6 text-center md:text-left">
       <img src={Cookie} alt="cookie" />
 
-      <p className="text-textNormal w-[50%]">
+      <p className="text-textNormal md:w-[50%]">
         We use third-party <span className="underline">cookies</span> to provide
         you the best user experience and for performance analytics.
       </p>
 
-      <div className="w-[40%] flex flex-col md:flex-row justify-between">
-        <button className="a-btn-inv w-[40%]">Deny</button>
+      <div className="md:w-[40%] flex  md:flex-row justify-between gap-6">
+        <button className="a-btn-inv md:w-[40%]">Deny</button>
 
-        <button className="a-btn w-[40%]">Accept</button>
+        <button className="a-btn md:w-[40%]">Accept</button>
       </div>
     </div>
   );
