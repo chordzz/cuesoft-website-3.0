2435b82322dea4ec9736084084aeaac820d04439
Olaife Olawore <52765542+chordzz@users.noreply.github.com>
2022-09-17 21:08:22 +0100

    Fix/cs 162 (#91)
    * Bump terser from 5.14.1 to 5.14.2
    
    Bumps [terser](https://github.com/terser/terser) from 5.14.1 to 5.14.2.
    - [Release notes](https://github.com/terser/terser/releases)
    - [Changelog](https://github.com/terser/terser/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/terser/terser/commits)
    
    ---
    updated-dependencies:
    - dependency-name: terser
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * finished investors page
    
    * added links to footer
    
    * fixed footer links review
    
    * added typeform function to start-a-project buttons
    
    * fixed about-page hero-img opacity issue
    
    * added slider functionality to about-page hero image
    
    * added switch question-answer onClick functionality to the FAQ section and created it's own component
    
    * fixed mobile responsiveness issue for FAQ section
    
    * still fixing merge/build conflicts
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Signed-off-by: Olaife Olawore <52765542+chordzz@users.noreply.github.com>
    Co-authored-by: Ibukun Dairo <ibukun.dairo@cuesoft.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/components/faq/FaqMain.tsx
@@ -0,0 +1,112 @@
+import React, { useEffect, useState } from "react";
+
+import ChevronRightNoBg from "../../assets/icons/chevron-right-no-bg.svg";
+
+import { questionsData } from "./FaqQuestions";
+
+export const FaqMain = () => {
+  type questionItemType = {
+    id: number;
+    question: string;
+    answer: string;
+    active: boolean;
+  };
+
+  const [activeQuestion, setActiveQuestion] = useState<questionItemType>(
+    questionsData[0]
+  );
+  const [questions, setQuestions] = useState<questionItemType[]>(questionsData);
+
+  const handleClick = (item: questionItemType) => {
+    setQuestions(
+      questionsData.map((questionItem) => {
+        questionItem.question === item.question
+          ? (questionItem.active = true)
+          : (questionItem.active = false);
+        return questionItem;
+      })
+    );
+    setActiveQuestion(item);
+  };
+
+  useEffect(() => {
+    // setActiveQuestion({
+    //     id: questions[0].id,
+    //     question: questions[0].question,
+    //     answer: questions[0].answer,
+    //     active: true
+    // });
+    handleClick(questions[0]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="my-container">
+      <h3 className="text-brightRed text-center dark:text-darkModeRed text-[24px] lg:text-[40px] font-bold mb-16">
+        Frequently Asked Questions
+      </h3>
+
+      <div className="hidden lg:flex relative items-center justify-end">
+        <div className="hidden lg:block w-[50%] bg-white dark:bg-darkModeHowCard absolute left-0 z-30 shadow-md">
+          <div className="flex flex-col text-[16px] text-textNormal dark:text-white">
+            {questions.map((questionItem, idx) => {
+              return (
+                <span
+                  key={questionItem.id}
+                  className={`${
+                    questionItem.active ? "faq-active" : "faq-inactive"
+                  } cursor-pointer`}
+                  onClick={() => handleClick(questionItem)}
+                >
+                  <span></span>
+                  <p>{questionItem.question}</p>
+                  <img src={ChevronRightNoBg} alt="Chevron right" />
+                </span>
+              );
+            })}
+          </div>
+        </div>
+
+        <div className="hidden min-h-[300px] lg:flex w-[60%]  text-textNormal dark:text-[#98A2B3] text-[16px] bg-[#FDE3E3] dark:bg-[#1C1C1C] rounded-lg shadow-lg py-12 pl-[15%] pr-12 items-center">
+          <div className="">
+            <p className="text-textBold dark:text-[#E0E1E7] font-bold text-[20px]">
+              {activeQuestion.question}
+            </p>
+            <p className="mt-8 ">{activeQuestion.answer}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Mobile View */}
+      <div className="lg:hidden">
+        {questions.map((questionItem) => {
+          return (
+            <div>
+              <span
+                key={questionItem.id}
+                className={`${
+                  questionItem.active ? "faq-active" : "faq-inactive"
+                } cursor-pointer`}
+                onClick={() => handleClick(questionItem)}
+              >
+                <span></span>
+                <p>{questionItem.question}</p>
+                <img src={ChevronRightNoBg} alt="Chevron right" />
+              </span>
+              <div
+                className={`${
+                  questionItem.active ? "block" : "hidden"
+                } min-h-[300px] text-textNormal dark:text-[#98A2B3] text-[16px] bg-[#FDE3E3] dark:bg-[#1C1C1C] rounded-lg shadow-sm py-8 px-6 my-6`}
+              >
+                <p className="text-textBold dark:text-[#E0E1E7] font-bold text-[20px]">
+                  {activeQuestion.question}
+                </p>
+                <p className="mt-8 ">{activeQuestion.answer}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-
-import ChevronRightNoBg from "../assets/icons/chevron-right-no-bg.svg";
+import { FaqMain } from "../components/faq/FaqMain";
 
 export const ServicesPage = () => {
   return (
@@ -9,78 +8,7 @@ export const ServicesPage = () => {
         id="faq"
         className="bg-bgLight2 dark:bg-darkModeBgLight min-h-screen py-20"
       >
-        <div className="my-container">
-          <h3 className="text-brightRed text-center dark:text-darkModeRed text-[24px] lg:text-[40px] font-bold mb-16">
-            Frequently Asked Questions
-          </h3>
-
-          <div className="flex relative items-center justify-end">
-            <div className="w-[50%] bg-white dark:bg-darkModeHowCard absolute left-0 z-30 shadow-md">
-              <ul className=" text-[16px] text-textNormal dark:text-white">
-                <li className="flex justify-between p-6 items-center bg-[#FAFBFF] dark:bg-[#0F0F0F] shadow-sm">
-                  <span className="w-[30px] h-[30px] bg-[#FF5151] dark:bg-[#FF6B6B] rounded-[100%]"></span>
-                  <p className="w-[80%] font-semibold">
-                    {" "}
-                    What is Cuesoft Enterprise ?{" "}
-                  </p>
-                  <img src={ChevronRightNoBg} alt="Chevron right" />
-                </li>
-                <li className="flex p-4 items-start ">
-                  <span className="w-[20px] h-[20px] bg-[#FFF1F1] dark:bg-[#221210] rounded-[100%]"></span>
-                  <p className="ml-6 w-[90%]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Odio.
-                  </p>
-                </li>
-                <li className="flex p-4 items-start ">
-                  <span className="w-[20px] h-[20px] bg-[#FFF1F1] dark:bg-[#221210] rounded-[100%]"></span>
-                  <p className="ml-6 w-[90%]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Odio.
-                  </p>
-                </li>
-                <li className="flex p-4 items-start ">
-                  <span className="w-[20px] h-[20px] bg-[#FFF1F1] dark:bg-[#221210] rounded-[100%]"></span>
-                  <p className="ml-6 w-[90%]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                    nam nibh.
-                  </p>
-                </li>
-                <li className="flex p-4 items-start ">
-                  <span className="w-[20px] h-[20px] bg-[#FFF1F1] dark:bg-[#221210] rounded-[100%]"></span>
-                  <p className="ml-6 w-[90%]">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Porta hendrerit sagittis est pulvinar integer congue.
-                    Viverra sagittis nisi.
-                  </p>
-                </li>
-              </ul>
-            </div>
-
-            <div className="w-[60%] text-textNormal dark:text-[#98A2B3] text-[16px] bg-[#FDE3E3] dark:bg-[#1C1C1C] rounded-lg relative shadow-lg py-12 pl-[15%] pr-12">
-              <div className="">
-                <p className="text-textBold dark:text-[#E0E1E7] font-bold text-[20px]">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Porttitor quam hendrerit non.
-                </p>
-                <p className="mt-8 ">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Facilisis tristique tellus mi risus id velit et consequat nam.
-                  Purus euismod potenti turpis magna leo, tortor sed tempus leo.
-                  Sollicitudin egestas etiam pellentesque egestas vulputate
-                  augue suspendisse.
-                  <br />
-                  <br />
-                  Laoreet aliquam tortor, vestibulum, at mi. Ut augue fermentum,
-                  sit iaculis ut at sed in. Tincidunt tellus sit vulputate morbi
-                  consequat at morbi sit quis. Velit ut nec in urna mattis
-                  egestas dapibus leo neque. Vitae, aliquam tortor sed faucibus.
-                  Porttitor sed orci eget felis.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <FaqMain />
       </section>
     </div>
   );
