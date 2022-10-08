bb8a582939fde7b2376b9a85f46cefc18572cee0
chordzz <olaifeolawore@gmail.com>
2022-10-08 11:18:48 +0100

    started adding mailchimp details to Newsletter component

--- src/components/newsletter-subscribe/Newsletter.tsx
+++ src/components/newsletter-subscribe/Newsletter.tsx
@@ -16,16 +16,22 @@ export const Newsletter = () => {
           <p className="mb-4 text-textNormal text-[12px] lg:text-2xl dark:text-white">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
-          <div className="flex flex-col gap-8 lg:flex-row">
-            <input
-              type="email"
-              placeholder="Your e-mail address"
-              className="lg:w-[321px] bg-[#FBFCFD] border border-[#132295]/[.1] py-3 px-8 rounded-[4px] dark:text-white dark:bg-[#161616]"
-            />
+          <div>
+            <form className="flex flex-col gap-8 lg:flex-row">
+              {/* <input type="hidden" name="u" value={process.env.REACT_APP_MAILCHIMP_U}/> */}
+              {/* <input type="hidden" name="id" value={process.env.REACT_APP_MAILCHIMP_ID}/> */}
+              <input
+                type="email"
+                placeholder="Your e-mail address"
+                className="lg:w-[321px] bg-[#FBFCFD] border border-[#132295]/[.1] py-3 px-8 rounded-[4px] dark:text-white dark:bg-[#161616]"
+              />
 
-            <button className="a-btn mx-auto w-full lg:ml-8 text-[13px] lg:w-[246px] lg:text-[17px]">
-              Subscribe to Mail
-            </button>
+              {/* <input type="hidden" name="ht" value="d28c3f4aff69a15419f00dea8fa1a2478a220866:MTY1NzAzNTU0NC43MzMx"/>
+              <input type="hidden" name="mc_signupsource" value="hosted"/> */}
+              <button className="a-btn mx-auto w-full lg:ml-8 text-[13px] lg:w-[246px] lg:text-[17px]">
+                Subscribe to Mail
+              </button>
+            </form>
           </div>
         </div>
       </div>
