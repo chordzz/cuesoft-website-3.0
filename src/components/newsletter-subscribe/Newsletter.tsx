cbfa103af484eaaa2af7ab05e5fc1bb22f7bc5c2
chordzz <olaifeolawore@gmail.com>
2022-10-10 13:36:34 +0100

    pausing dev to fix cookie pipeline

--- src/components/newsletter-subscribe/Newsletter.tsx
+++ src/components/newsletter-subscribe/Newsletter.tsx
@@ -17,18 +17,45 @@ export const Newsletter = () => {
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
           </p>
           <div>
-            <form className="flex flex-col gap-8 lg:flex-row">
+            <form
+              className="flex flex-col gap-8 lg:flex-row validate"
+              action="https://cuesoft.us14.list-manage.com/subscribe/post?u=cf78d03209bc58066c06332ab&amp;id=dcd37aeaf4&amp;f_id=001a8de0f0"
+              method="post"
+              id="mc-embedded-subscribe-form"
+              name="mc-embedded-subscribe-form"
+              target="_blank"
+            >
               {/* <input type="hidden" name="u" value={process.env.REACT_APP_MAILCHIMP_U}/> */}
               {/* <input type="hidden" name="id" value={process.env.REACT_APP_MAILCHIMP_ID}/> */}
               <input
                 type="email"
                 placeholder="Your e-mail address"
-                className="lg:w-[321px] bg-[#FBFCFD] border border-[#132295]/[.1] py-3 px-8 rounded-[4px] dark:text-white dark:bg-[#161616]"
+                className="lg:w-[321px] bg-[#FBFCFD] border border-[#132295]/[.1] py-3 px-8 rounded-[4px] dark:text-white dark:bg-[#161616] required email"
+                name="EMAIL"
+                id="mce-EMAIL"
+                required
               />
+
+              <div id="mce-responses" className="clear foot">
+                <div
+                  className="response"
+                  id="mce-error-response"
+                  style={{ display: "none" }}
+                ></div>
+                <div
+                  className="response"
+                  id="mce-success-response"
+                  style={{ display: "none" }}
+                ></div>
+              </div>
 
               {/* <input type="hidden" name="ht" value="d28c3f4aff69a15419f00dea8fa1a2478a220866:MTY1NzAzNTU0NC43MzMx"/>
               <input type="hidden" name="mc_signupsource" value="hosted"/> */}
-              <button className="a-btn mx-auto w-full lg:ml-8 text-[13px] lg:w-[246px] lg:text-[17px]">
+              <button
+                type="submit"
+                id="mc-embedded-subscribe"
+                className="a-btn mx-auto w-full lg:ml-8 text-[13px] lg:w-[246px] lg:text-[17px]"
+              >
                 Subscribe to Mail
               </button>
             </form>
